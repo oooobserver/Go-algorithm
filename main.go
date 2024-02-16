97430fb7000aaa3a14a86340997807fb2ca5d6d4
shenye <shenye159357852@gmail.com>
2024-02-16 12:00:27 +0800

    no more main.go

--- main.go
+++ main.go
@@ -29,5 +29,36 @@ func print(l *ListNode) {
 	}
 }
 
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := (l + r + 1) / 2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[l] < nums[mid] {
+			if target > nums[mid] || target < nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+
+		if nums[mid] < nums[r] {
+			if target > nums[r] || target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	r := search(nums, 0)
+	fmt.Println(r)
 }
